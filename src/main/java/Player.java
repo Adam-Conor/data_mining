96da21111276621d318f57fd6883c8e161fb93a7
Conor Smyth <cnrsmyth@gmail.com>
2015-12-01 17:41:26 +0000

    Added columns to be added to player. COmments, getters and setters to be added later

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,13 +1,22 @@
 /**
  * Class for holding information about a player
  * All work is our own
+ * TODO Add getters and setters for new fields
  * @author Conor Smyth <conor.smyth39@mail.dcu.ie>
  * @author Adam O'Flynn <adam.oflynn7@mail.dcu.ie>
  * @since 2015-11-30
  */
 public class Player {
 	private String name;
+
 	private double avgPoints;
+	private double rank;
+	private double prevRank;
+	private double totalPoints;
+	private double divisorEvents;
+	private double actualEvents;
+	private double totalPointsGained;
+	private double totalPointsLost;
 
 	public Player() {}
 
@@ -16,9 +25,17 @@ public class Player {
 	 * @param name name of the player as a string
 	 * @param avgPoints average points of the player
 	 */
-	public Player(String name, double avgPoints) {
+	public Player(String name, double avgPoints, double rank, double prevRank, 
+			double totalPoints, double divisorEvents, double actualEvents, double totalPointsGained, double totalPointsLost) {
 		this.name = name;
 		this.avgPoints = avgPoints;
+		this.rank = rank;
+		this.prevRank = prevRank;
+		this.totalPoints = totalPoints;
+		this.divisorEvents = divisorEvents;
+		this.actualEvents = actualEvents;
+		this.totalPointsGained = totalPointsGained;
+		this.totalPointsLost = totalPointsLost;
 	}
 
 	/**
@@ -67,6 +84,27 @@ public class Player {
 			builder.append(", ");
 			builder.append("Average Points: ");
 			builder.append(avgPoints);
+			builder.append(", ");
+			builder.append("Rank: ");
+			builder.append(rank);
+			builder.append(", ");
+			builder.append("Previous Rank: ");
+			builder.append(prevRank);
+			builder.append(", ");
+			builder.append("Total Points: ");
+			builder.append(totalPoints);
+			builder.append(", ");
+			builder.append("Divisor Events: ");
+			builder.append(divisorEvents);
+			builder.append(", ");
+			builder.append("Actual Events: ");
+			builder.append(actualEvents);
+			builder.append(", ");
+			builder.append("Total Points Gained: ");
+			builder.append(totalPointsGained);
+			builder.append(", ");
+			builder.append("Total Points Lost: ");
+			builder.append(totalPointsLost);
 			builder.append("]");
 
 			return builder.toString();
